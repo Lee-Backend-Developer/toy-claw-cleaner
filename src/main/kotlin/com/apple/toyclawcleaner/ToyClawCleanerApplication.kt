3a2dda0ea9539daeb74eea500b6f81978f15154e
lee <eotmd1016@gmail.com>
2025-10-21 00:28:06 +0900

    [git] : init

---
+++ src/main/kotlin/com/apple/toyclawcleaner/ToyClawCleanerApplication.kt
@@ -0,0 +1,11 @@
+package com.apple.toyclawcleaner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToyClawCleanerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ToyClawCleanerApplication>(*args)
+}
---
+++ src/test/kotlin/com/apple/toyclawcleaner/ToyClawCleanerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.apple.toyclawcleaner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToyClawCleanerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
