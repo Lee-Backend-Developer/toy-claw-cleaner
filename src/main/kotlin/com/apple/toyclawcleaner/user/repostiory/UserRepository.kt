03dcb8746cbddfb1af5fa4f983493b2618d8e739
lee <eotmd1016@gmail.com>
2025-10-21 02:30:58 +0900

    [user] : ID가 Long에서 UUID로 변경

--- src/main/kotlin/com/apple/toyclawcleaner/user/repostiory/UserRepository.kt
+++ src/main/kotlin/com/apple/toyclawcleaner/user/repostiory/UserRepository.kt
@@ -2,6 +2,7 @@ package com.apple.toyclawcleaner.user.repostiory
 
 import com.apple.toyclawcleaner.user.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
 
-interface UserRepository : JpaRepository<UserEntity, Long> {
+interface UserRepository : JpaRepository<UserEntity, UUID> {
 }
